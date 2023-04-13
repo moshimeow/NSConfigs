c6d63b29ec2acd992d8ff70a3abe1765a15918c3
Moshi Turner <moses@collabora.com>
2023-04-13 06:12:39 -0500

    Add calibration files for Mateo's headset

---
+++ mateo/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+NS_CONFIG_PATH=$(pwd)/NSConfig.json SLAM_CONFIG=$(pwd)/slam_config_no_gui.toml XRT_DEBUG_GUI=1 XRT_COMPOSITOR_COMPUTE=1 SLAM_SUBMIT_FROM_START=1 monado-service
